f123faf0c5abf041f1e052ff877849b1624914c1
nelicepriatniy <92029297+nelicepriatniy@users.noreply.github.com>
2022-11-25 13:55:01 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,40 @@
+
+
+const swiper1 = new Swiper('.slider-name', {
+    // Optional parameters
+    direction: 'vertical',
+    mousewheel: {
+        eventsTarget: 'body',
+      },
+    speed: 800,
+    centeredSlides: true,
+    slidesPerView: 3,
+    spaceBetween: 150,
+    allowTouchMove: false,
+});
+const swiper2 = new Swiper('.slider-name-color', {
+    // Optional parameters
+    direction: 'vertical',
+    mousewheel: {
+        eventsTarget: 'body',
+      },
+    speed: 800,
+    centeredSlides: true,
+    slidesPerView: 1,
+    spaceBetween: 150,
+    allowTouchMove: false,
+});
+
+const swiper = new Swiper('.slider1', {
+    // Optional parameters
+    direction: 'vertical',
+    mousewheel: {
+        eventsTarget: 'body',
+      },
+    speed: 800,
+    allowTouchMove: false,
+});
+
+function dohref (){
+    let hreffrom = document.querySelectorAll('.namehref');
+}
